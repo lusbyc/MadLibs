75fb873f0d4e5b1ec60ff78f0c0a231526bc4333
lusbyc <43541354+lusbyc@users.noreply.github.com>
2021-11-11 00:16:00 -0500

    Add files via upload
    First commit

---
+++ main.py
@@ -0,0 +1,14 @@
+
+adjective = input("Enter an adjective: ")
+noun = input("Enter a noun: ") 
+past_tense_verb = input("Enter a past tense verb: ")
+adverb = input("Enter an adverb: ")
+verb = input("Enter a verb: ")
+
+print(f"""Today I went to the zoo. I saw a {adjective} {noun} jumping up and down in its tree. 
+He {past_tense_verb} {adverb} through the large tunnel that led to its {adjective} {noun}. I got 
+some peanuts and passed them through the cage to a gigantic gray {noun} towering 
+above my head. Feeding that animal made me hungry. I went to get a {adjective} scoop 
+of ice cream. It filled my stomach. Afterwards I had to {verb} {adverb} to catch our 
+bus. When I got home I {past_tense_verb} my mom for a {adjective} day at the zoo.""")
+
